9bfd1aafea6b39927ec591f00ab6fe5973258f29
Jose Piñeiro <infjmpineiro@elorrieta-erreakamari.com>
2025-12-24 10:47:24 +0100

    Añadido hipotenu.java

---
+++ Hipotenu.java
@@ -0,0 +1,27 @@
+package evaluacion1;
+
+import java.util.Scanner;
+
+public class Hipotenu {
+
+	public static double pedirNumero (String mensaje, Scanner teclado) {
+		System.out.print(mensaje);
+		double numero = teclado.nextDouble();
+		return numero;
+	}
+	
+	public static double calcHipotenusa (double cateto1, double cateto2) {
+		double hipo = Math.sqrt(Math.pow(cateto1, 2) + Math.pow(cateto2, 2));
+		return hipo;
+	}
+	
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner (System.in);
+		double cateto1 = pedirNumero ("Introduzca el primer cateto: ", teclado);
+		double cateto2 = pedirNumero ("Introduzca el segundo cateto: ", teclado);
+		double hipotenusa = calcHipotenusa (cateto1, cateto2);
+		System.out.printf ("La hipotenusa del triángulo mide %.2f", hipotenusa);
+		teclado.close();
+	}
+
+}
